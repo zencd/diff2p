4bdf246ec7fac93c05e71ef1efac67caa125e8b7
pasza <z@z.com>
2023-09-10 21:39:46 +0300

    style

--- diff2p.py
+++ diff2p.py
@@ -6,6 +6,9 @@ import re
 import sys
 import subprocess
 
+SED_ACTION_REGEX = re.compile('(\d+)(,(\d+))?' + '([cda])' + '(\d+)(,(\d+))?')
+ui: 'ConsoleUI' = None
+
 
 def read_process_output(args):
     proc = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -50,7 +53,6 @@ class ConsoleUI:
         self.wrap = False
         self.left_width = w1
         self.right_width = w2
-        # self.format_str = '|\033[93m%%-%ds\033[0m|%%-%ds|' % (self.left_width, self.right_width)
         self.format_left = '%%-%ds' % self.left_width
         self.format_right = '%%-%ds' % self.right_width
 
@@ -62,15 +64,11 @@ class ConsoleUI:
 
         self.normal_delim_left = '|'
         self.normal_delim_right = ' '
-        # self.exceeded_delim = '\033[91m}\033[0m'
         self.exceeded_delim = '\033[41;37m}\033[0m'
 
         self.tab_replacement = ' ' * 4
 
     def print_two_panels(self, line1, line2, sed_change=False):
-        # print self.format_str % (line1, line2)
-        # sys.stdout.write('|')
-
         line1 = line1 if line1 is None else line1.replace('\t', self.tab_replacement)
         line2 = line2 if line2 is None else line2.replace('\t', self.tab_replacement)
 
@@ -89,9 +87,7 @@ class ConsoleUI:
         if line1:
             s = self.format_left % line1
             if sed_change:
-                # s = '\033[33m%s\033[0m' % s
                 s = '\033[46;30m%s\033[0m' % s
-                # s = '\033[7m%s\033[0m' % s
             sys.stdout.write(s)
         else:
             sys.stdout.write(self.sync_line_left)
@@ -101,7 +97,6 @@ class ConsoleUI:
         if line2:
             s = self.format_right % line2
             if sed_change:
-                # s = '\033[33m%s\033[0m' % s # 93m
                 s = '\033[46;30m%s\033[0m' % s  # 93m
             sys.stdout.write(s)
         else:
@@ -126,8 +121,10 @@ def sed_change(start1, end1, start2, end2, in1, in2):
     range1 = end1 - start1 + 1
     range2 = end2 - start2 + 1
 
+    def continue_1_2(lines_read, action_start):
+        return lines_read + 1 < action_start
+
     # print equal lines before current sed action
-    continue_1_2 = lambda lines_read, action_start: lines_read + 1 < action_start
     pass_equal_lines_before_current_action(start1, start2, in1, in2, continue_1_2, continue_1_2)
 
     # print actual difference
@@ -153,8 +150,12 @@ def sed_append(start1, end1, start2, end2, in1, in2):
 
 
 def _sed_delete_or_append(start1, end1, start2, end2, in1, in2, is_delete):
-    continue_if_1 = lambda lines_read, action_start: lines_read + 1 < action_start
-    continue_if_2 = lambda lines_read, action_start: lines_read + 0 < action_start
+    def continue_if_1(lines_read, action_start):
+        return lines_read + 1 < action_start
+
+    def continue_if_2(lines_read, action_start):
+        return lines_read + 0 < action_start
+
     if not is_delete:
         (continue_if_1, continue_if_2) = (continue_if_2, continue_if_1)
     pass_equal_lines_before_current_action(start1, start2, in1, in2, continue_if_1, continue_if_2)
@@ -190,7 +191,6 @@ def parse_diff(diff_text, input1, input2):
     in1 = make_file_reader(input1)
     in2 = make_file_reader(input2)
     diff_reader = make_string_reader(diff_text)
-    SED_ACTION_REGEX = re.compile('(\d+)(,(\d+))?' + '([cda])' + '(\d+)(,(\d+))?')
     while True:
         line = diff_reader.read_line()
         if not line: break
@@ -203,11 +203,12 @@ def parse_diff(diff_text, input1, input2):
             end2 = make_int_or_none(m.group(7))
 
             # convert '1c4' to '1,1c4,4'
-            if end1 is None: end1 = start1
-            if end2 is None: end2 = start2
+            if end1 is None:
+                end1 = start1
+            if end2 is None:
+                end2 = start2
 
             sed_commands[sed_cmd](start1, end1, start2, end2, in1, in2)
-            # print 'matched:', sed_cmd, start1, end1, start2, end2
         elif re.match('[<>-].*', line):
             pass
         else:
@@ -215,19 +216,22 @@ def parse_diff(diff_text, input1, input2):
     print_tails(in1, in2)
 
 
-if __name__ == '__main__':
+def main():
     input1 = sys.argv[1]
     input2 = sys.argv[2]
 
     try:
         size = read_process_output(['stty', 'size'])
-        # print(f"size: {size}")
         width = int(size.strip().split(' ')[1])
     except IndexError:
         # stty not available in IDE
         width = 80
 
+    global ui
     ui = ConsoleUI(width)
-
     diff_text = read_process_output(['diff', input1, input2])
     parse_diff(diff_text, input1, input2)
+
+
+if __name__ == '__main__':
+    main()
